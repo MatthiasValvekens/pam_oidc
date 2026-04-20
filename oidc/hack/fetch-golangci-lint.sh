2e93a4cc9f627aeafc650025b2deb3fc9a5065bc
Matthias Valvekens <matthias@mvalvekens.be>
2026-04-20 08:14:00 +0200

    Update golangci

--- oidc/hack/fetch-golangci-lint.sh
+++ oidc/hack/fetch-golangci-lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-golangci_lint_version="1.23.8"
+golangci_lint_version="2.11.4"
 golangci_lint_sha256="unknown" # set in platform block below
 
 goarch=amd64 # it's 2020
@@ -9,10 +9,10 @@ goos="unknown"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   goos="linux"
-  golangci_lint_sha256="9c95f7860cdddff92ba7eb0d765267bb0c868e8a991100b75f4caa8818f92f96"
+  golangci_lint_sha256="200c5b7503f67b59a6743ccf32133026c174e272b930ee79aa2aa6f37aca7ef1"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   goos="darwin"
-  golangci_lint_sha256="de309a84af016cc751528361bfa4403a011850ad073585f500ed463b35e66daf"
+  golangci_lint_sha256="c900d4048db75d1edfd550fd11cf6a9b3008e7caa8e119fcddbc700412d63e60"
 fi
 
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
